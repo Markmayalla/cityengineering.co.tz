5018c86af28172c128a020aac60141bc92128b8e
Mark - DopestGeek <markmayalla@gmail.com>
2020-06-28 12:33:59 +0300

    Project Initialization and Setup, WebController and Web Layout drafts created

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.wrapper')
+@section('layout')
+    <h1>Header</h1>
+    @yield('content')
+    <h1>Footer</h1>
+@endsection
---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.web')
+@section('content')
+    <h2>This is my Home Page</h2>
+@endsection
